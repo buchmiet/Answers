e3f1a4ae302aa24c36a3c51c7407ebd353f4e26a
plant666 <plant@o2.pl>
2024-10-12 18:04:05 +0100

    added tests

---
+++ Answers.Tests/AnswerGenericTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Answers;
+using System;
+
+namespace Answers.Tests
+{
+   
+
+    public class AnswerGenericTests
+    {
+        [Fact]
+        public void WithValue_ShouldSetAndReturnCorrectValue()
+        {
+            // Arrange
+            var answer = new Answer<int>();
+
+            // Act
+            answer.WithValue(42);
+
+            // Assert
+            Assert.Equal(42, answer.GetValue());
+        }
+
+        [Fact]
+        public void GetValue_ShouldThrowIfValueNotSet()
+        {
+            // Arrange
+            var answer = new Answer<int>();
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => answer.GetValue());
+        }
+
+        [Fact]
+        public void Out_ShouldReturnSuccessAndSetValue()
+        {
+            // Arrange
+            var answer = new Answer<int>().WithValue(42);
+
+            // Act
+            bool success = answer.Out(out int result);
+
+            // Assert
+            Assert.True(success);
+            Assert.Equal(42, result);
+        }
+    }
+
+}
---
+++ Answers.Tests/AnswerTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Answers;
+
+namespace Answers.Tests
+{
+  
+
+    public class AnswerTests
+    {
+        [Fact]
+        public void Prepare_ShouldCreateSuccessAnswerWithMessage()
+        {
+            // Arrange
+            string action = "Test Action";
+
+            // Act
+            var answer = Answer.Prepare(action);
+
+            // Assert
+            Assert.True(answer.IsSuccess);
+            Assert.Equal(action, answer.Message);
+        }
+
+        [Fact]
+        public void TimedOut_ShouldSetTimedOutState()
+        {
+            // Act
+            var answer = Answer.TimedOut();
+
+            // Assert
+            Assert.True(answer.IsTimedOut);
+        }
+
+        [Fact]
+        public void Attach_ShouldCombineMessagesAndStates()
+        {
+            // Arrange
+            var answer1 = Answer.Prepare("Action 1");
+            var answer2 = Answer.Prepare("Action 2");
+
+            // Act
+            var combinedAnswer = answer1.Attach(answer2);
+
+            // Assert
+            Assert.True(combinedAnswer.IsSuccess);
+            Assert.Contains("Action 1", combinedAnswer.Message);
+            Assert.Contains("Action 2", combinedAnswer.Message);
+        }
+    }
+
+}
