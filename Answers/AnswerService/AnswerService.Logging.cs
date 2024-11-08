9aac0d0d3840c34e65fb110c55c0f12e6296a68e
plant666 <plant@o2.pl>
2024-11-08 17:53:54 +0000

    fix: fix for logging in answerservice

--- Answers/AnswerService/AnswerService.Logging.cs
+++ Answers/AnswerService/AnswerService.Logging.cs
@@ -7,17 +7,17 @@ namespace Answers
     {
         public void LogWarning(string message)
         {
-            _logger.LogWarning(message);
+            _logger?.LogWarning(message);
         }
 
         public void LogError(string message)
         {
-            _logger.LogError(message);
+            _logger?.LogError(message);
         }
 
         public void LogInfo(string message)
         {
-            _logger.LogInformation(message);
+            _logger?.LogInformation(message);
         }
     }
 }
